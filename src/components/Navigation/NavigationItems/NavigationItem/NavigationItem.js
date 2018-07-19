7227b6a89239e179fe891b6a3f9bb85eff52ce43
Simone Sabbatini <simone.sabbatini@bynd.com>
2018-07-19 12:00:34 +0100

    Added side drawer component and some style fixes

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
-    <li className={classes.NavigationItems}>
+    <li className={classes.NavigationItem}>
         <a
             href={props.link}
             className={props.active ? classes.active : null}>
